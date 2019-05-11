37edd8bffe095fad978e696aee4869ec5c68ba76
nursh <nsheikhmohammed@yahoo.com>
2019-05-11 16:11:09 -0400

    Added PetType to bootstrap. Closes #33

--- pet-clinic-web/src/main/java/com/nursh/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/nursh/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.nursh.petclinic.bootstrap;
 
 import com.nursh.petclinic.model.Owner;
+import com.nursh.petclinic.model.PetType;
 import com.nursh.petclinic.model.Vet;
 import com.nursh.petclinic.service.OwnerService;
+import com.nursh.petclinic.service.PetTypeService;
 import com.nursh.petclinic.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,31 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
+
+
+
 
     @Override
     public void run(String... args) throws Exception {
+
+        /******************
+         *
+         * PetTypes
+         ******************/
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         /******************
          *
