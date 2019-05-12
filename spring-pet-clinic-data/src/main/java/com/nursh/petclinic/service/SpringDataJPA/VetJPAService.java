6cb15d2a41c2ddc86f3117c61566c59187693be8
nursh <nsheikhmohammed@yahoo.com>
2019-05-12 17:31:25 -0400

    Added JPA service for the Vet class. Closes #41

---
+++ spring-pet-clinic-data/src/main/java/com/nursh/petclinic/service/SpringDataJPA/VetJPAService.java
@@ -0,0 +1,44 @@
+package com.nursh.petclinic.service.SpringDataJPA;
+
+import com.nursh.petclinic.model.Vet;
+import com.nursh.petclinic.repositories.VetRepository;
+import com.nursh.petclinic.service.VetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class VetJPAService implements VetService {
+    
+    private final VetRepository vetRepository;
+
+    public VetJPAService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
