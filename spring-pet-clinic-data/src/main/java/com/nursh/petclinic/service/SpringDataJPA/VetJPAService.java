77898df35d856fb864c579f5a53454330f66fcee
nursh <nsheikhmohammed@yahoo.com>
2019-05-12 18:10:56 -0400

    Added JPA service for the Vet and Pet class. Closes #44, Closes #42

--- spring-pet-clinic-data/src/main/java/com/nursh/petclinic/service/SpringDataJPA/VetJPAService.java
+++ spring-pet-clinic-data/src/main/java/com/nursh/petclinic/service/SpringDataJPA/VetJPAService.java
@@ -3,10 +3,14 @@ package com.nursh.petclinic.service.SpringDataJPA;
 import com.nursh.petclinic.model.Vet;
 import com.nursh.petclinic.repositories.VetRepository;
 import com.nursh.petclinic.service.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class VetJPAService implements VetService {
     
     private final VetRepository vetRepository;
